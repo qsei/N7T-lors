48c9a11532a0d444e07f44405f80fa9d67fc89ca
qsei <eofoxo@gmail.com>
2023-07-29 04:37:40 +0700

    add springboot data

---
+++ src/main/java/com/lors/LORs/LoRsApplication.java
@@ -0,0 +1,13 @@
+package com.lors.LORs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoRsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lors/LORs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lors.LORs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoRsApplication.class);
+	}
+
+}
